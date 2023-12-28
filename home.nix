401edd32a312f15f300f33576f62cc7409ac5d0b
derekagraham78 <derekagraham78@icloud.com>
2023-12-27 19:28:41 -0600

    Add existing project files prior to the push to GitHub.

--- home.nix
+++ home.nix
@@ -1,44 +1,44 @@
 { config, pkgs, ... }:
 
 {
-  # TODO please change the username & home direcotry to your own
-  home.username = "dgraham";
-  home.homeDirectory = "/home/dgraham";
-  # basic configuration of git, please change to your own
-  programs.git = {
-    enable = true;
-    userName = "derekagraham78";
-    userEmail = "derekagraham78@icloud.com";
-  };
+# TODO please change the username & home direcotry to your own
+home.username = "dgraham";
+home.homeDirectory = "/home/dgraham";
+# basic configuration of git, please change to your own
+programs.git = {
+enable = true                            ;
+userName = "derekagraham78";
+userEmail = "derekagraham78@icloud.com";
+}                                        ;
 
-  # Packages that should be installed to the user profile.
-  home.packages = with pkgs; [
-    # here is some command line tools I use frequently
-    # feel free to add your own or remove some of them
+# Packages that should be installed to the user profile.
+home.packages = with pkgs ; [
+# here is some command line tools I use frequently
+# feel free to add your own or remove some of them
 
-    # archives
+# archives
 	zip
 	xz
 	unzip
 	p7zip
 
-    # utils
+# utils
 	ripgrep # recursively searches directories for a regex pattern
-	jq # A lightweight and flexible command-line JSON processor
-	yq-go # yaml processer https://github.com/mikefarah/yq
-	fzf # A command-line fuzzy finder
+	jq      # A lightweight and flexible command-line JSON processor
+	yq-go   # yaml processer https://github.com/mikefarah/yq
+	fzf     # A command-line fuzzy finder
 
-    # networking tools
-	mtr # A network diagnostic tool
- 	iperf3
- 	dnsutils  # `dig` + `nslookup`
-    	ldns # replacement of `dig`, it provide the command `drill`
-    	aria2 # A lightweight multi-protocol & multi-source command-line download utility
- 	socat # replacement of openbsd-netcat
-	nmap # A utility for network discovery and security auditing
-	ipcalc  # it is a calculator for the IPv4/v6 addresses
+# networking tools
+	mtr      # A network diagnostic tool
+	iperf3
+	dnsutils # `dig` + `nslookup`
+	ldns     # replacement of `dig`, it provide the command `drill`
+	aria2    # A lightweight multi-protocol & multi-source command-line download utility
+	socat    # replacement of openbsd-netcat
+	nmap     # A utility for network discovery and security auditing
+	ipcalc   # it is a calculator for the IPv4/v6 addresses
 
-    # misc
+# misc
 	cowsay
 	file
 	which
@@ -49,30 +49,30 @@
 	zstd
 	gnupg
 
-    # nix related
-    #
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
+# nix related
+#
+# it provides the command `nom` works just like `nix`
+# with more details log output
 	nix-output-monitor
 
-    # productivity
-	hugo # static site generator
-	glow # markdown previewer in terminal
+# productivity
+	hugo  # static site generator
+	glow  # markdown previewer in terminal
 	btop  # replacement of htop/nmon
 	iotop # io monitoring
 	iftop # network monitoring
 
-    # system call monitoring
+# system call monitoring
 	strace # system call monitoring
 	ltrace # library call monitoring
-	lsof # list open files
+	lsof   # list open files
 
-    # system tools
+# system tools
 	sysstat
-	lm_sensors # for `sensors` command
+	lm_sensors                     # for `sensors` command
 	ethtool
-	pciutils # lspci
-	usbutils # lsusb
+	pciutils                       # lspci
+	usbutils                       # lsusb
 	gh
 	vivaldi-ffmpeg-codecs
 	xfce.thunar
@@ -122,19 +122,18 @@
 	python311Packages.qpageview
 	libsForQt5.qt5.wrapQtAppsHook	
 	neofetch
-  ];
+]                               ;
 
+# This value determines the home Manager release that your
+# configuration is compatible with. This helps avoid breakage
+# when a new home Manager release introduces backwards
+# incompatible changes.
+#
+# You can update home Manager without changing this value. See
+# the home Manager release notes for a list of state version
+# changes in each release.
+home.stateVersion = "23.11";
 
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "23.11";
-
-  # Let home Manager install and manage itself.
-  programs.home-manager.enable = true;
+# Let home Manager install and manage itself.
+programs.home-manager.enable = true ;
 }
