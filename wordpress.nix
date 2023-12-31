27484744dc69ca85cacf17e79e1a1e285559b76b
derekagraham78 <derekagraham78@icloud.com>
2023-12-30 23:53:37 -0600

    Add existing project files prior to the push to GitHub.

--- wordpress.nix
+++ wordpress.nix
@@ -7,8 +7,13 @@
 services.wordpress.sites."papalpenguin.com" = {};
 services.wordpress.sites."papalpenguin.com".virtualHost.documentRoot = "/var/www/papalpenguin.com";
 services.wordpress.sites."papalpenguin.com".virtualHost.addSSL = true;
-services.wordpress.sites."papalpenguin.com".virtualHost.listen.*.ssl = true;
-
+services.wordpress.sites."papalpenguin.com".virtualHost.listen [
+{
+    ip = "192.168.4.60";
+    port = 443;
+    ssl = true;
+}
+];
 
 services.wordpress.sites."papalpenguin.com".settings = {
 WP_HOME = "https://www.papalpenguin.com";
