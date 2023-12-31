6d4e093799751315cae6575ac8ee20524204eeb7
derekagraham78 <derekagraham78@icloud.com>
2023-12-31 02:25:33 -0600

    Add existing project files prior to the push to GitHub.

--- wordpress.nix
+++ wordpress.nix
@@ -4,50 +4,7 @@
 { config, lib, pkgs, ... }:
 
 
-let 
-domain = "papalpenguin.com";
-  # Auxiliary functions
-  fetchPackage = { name, version, hash, isTheme }:
-    pkgs.stdenv.mkDerivation rec {
-      inherit name version hash;
-      src = let type = if isTheme then "theme" else "plugin";
-      in pkgs.fetchzip {
-        inherit name version hash;
-        url = "https://downloads.wordpress.org/${type}/${name}.${version}.zip";
-      };
-      installPhase = "mkdir -p $out; cp -R * $out/";
-    };
-
-  fetchPlugin = { name, version, hash }:
-    (fetchPackage {
-      name = name;
-      version = version;
-      hash = hash;
-      isTheme = false;
-    });
-
-  fetchTheme = { name, version, hash }:
-    (fetchPackage {
-      name = name;
-      version = version;
-      hash = hash;
-      isTheme = true;
-    });
-
-  # Plugins
-  google-site-kit = (fetchPlugin {
-    name = "google-site-kit";
-    version = "1.103.0";
-    hash = "sha256-8QZ4XTCKVdIVtbTV7Ka4HVMiUGkBYkxsw8ctWDV8gxs=";
-  });
-
-  # Themes
-  astra = (fetchTheme {
-    name = "astra";
-    version = "4.5.2";
-    hash = "sha256-X3Jv2kn0FCCOPgrID0ZU8CuSjm/Ia/d+om/ShP5IBgA=";
-  });
-in {
+{
 services.wordpress.sites."papalpenguin.com".virtualHost.enableACME = true;
 services.wordpress.sites."papalpenguin.com" = {};
 services.wordpress.sites."papalpenguin.com".virtualHost.documentRoot = "/var/www/papalpenguin.com";
