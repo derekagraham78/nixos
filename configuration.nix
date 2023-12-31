e9529aa2e15608935dca3a257db8c7d72504728c
derekagraham78 <derekagraham78@icloud.com>
2023-12-30 21:23:46 -0600

    Add existing project files prior to the push to GitHub.

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,7 @@
 imports =
 [                                                                       # Include the results of the hardware scan.
 	./hardware-configuration.nix
+	./mysql.nix
 	./mysql.papalpenguin.com
 ]                                                                       ;
 # Home Manager
---
+++ mysql.nix
@@ -0,0 +1,6 @@
+{ config, pkgs, ... }: {
+ 
+  # Enable the mysql service.
+  services.mysql.enable = true;
+  services.mysql.package = pkgs.mariadb;
+}
