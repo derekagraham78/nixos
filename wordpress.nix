d89c8d88e8f5c1e5b47a5dc765550ebb0d49c6de
derekagraham78 <derekagraham78@icloud.com>
2023-12-30 23:27:56 -0600

    Add existing project files prior to the push to GitHub.

--- wordpress.nix
+++ wordpress.nix
@@ -17,7 +17,6 @@ services.wordpress.sites."papalpenguin.com".virtualHost.serverAliases = [
 "papalpenguin.com"
 ];
 services.wordpress.sites."papalpenguin.com".virtualHost.locations."/".index = "index.php index.html";
-];
 services.wordpress.sites."papalpenguin.com".virtualHost
 {
   adminAddr = "derek@papalpenguin.com";
