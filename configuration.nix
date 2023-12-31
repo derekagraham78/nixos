3321cbeb3d199600bf5baa23e468acc3cda7f936
derekagraham78 <derekagraham78@icloud.com>
2023-12-31 04:37:50 -0600

    Add existing project files prior to the push to GitHub.

--- configuration.nix
+++ configuration.nix
@@ -136,8 +136,7 @@ services.phpfpm.pools.mypool = {
 	nixpkgs.config.permittedInsecurePackages = [ "python-2.7.18.7" ]       ;
 nixpkgs.overlays = 
 [ 
-(final: prev: { libao = prev.libao.override { usePulseAudio = final.config.pulseaudio or (final.lib.meta.availableOn final.stdenv.hostPlatform final.libpulseaudio); }; 
-); }; 
+(final: prev: { libao = prev.libao.override { usePulseAudio = final.config.pulseaudio) or (final.lib.meta.availableOn final.stdenv.hostPlatform final.libpulseaudio); };  }; 
 ];
 # Enable sound with pipewire.
 	sound.enable = true;
