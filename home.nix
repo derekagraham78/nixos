6d877ef9b9ba48fa50caecc91893686956a80077
derekagraham78 <derekagraham78@icloud.com>
2024-01-02 03:13:10 -0600

    Flake rebuilt at: 01/02/24 03:13:10: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/home.nix b/home.nix # index 190ba39..6585cb4 100644 # --- a/home.nix # +++ b/home.nix # @@ -5,6 +5,31 @@ # ... # }: { # # hyprland # + wayland.windowManager.hyprland.settings = { # + bind = # + [ ] # + ( # + # workspaces # + # binds $mod + [shift +] {1..10} to [move to] workspace {1..10} # + builtins.concatLists (builtins.genList ( # + x: let # + ws = let # + c = (x + 1) / 10; # + in # + builtins.toString (x + 1 - (c backups configuration.nix flake.lock flake.nix hardware-configuration.nix home.nix machineid mysql.nix vscode.nix wordpress.nix 10)); # + in [ # + "$mod, ${ws}, workspace, ${toString (x + 1)}" # + "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}" # + ] # + ) # + 10) # + ); # + }; # +} # + wayland.windowManager.hyprland.plugins = [ # + inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars # + "/absolute/path/to/plugin.so" # +]; # home.file."~/.config/hypr/hyprland.conf".text = '' # decoration { # shadow_offset = 0 5
    Automatically committed and pushed

--- home.nix
+++ home.nix
@@ -5,6 +5,31 @@
   ...
 }: {
   # hyprland
+  wayland.windowManager.hyprland.settings = {
+    bind =
+      [ ]
+  (
+        # workspaces
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
+  };
+}
+  wayland.windowManager.hyprland.plugins = [
+  inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
+  "/absolute/path/to/plugin.so"
+];
   home.file."~/.config/hypr/hyprland.conf".text = ''
     decoration {
     	shadow_offset = 0 5
