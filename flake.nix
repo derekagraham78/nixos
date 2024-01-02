a76ffd3ac14f44e965caebc02623e62f8489bbac
derekagraham78 <derekagraham78@icloud.com>
2024-01-02 03:00:01 -0600

    Flake rebuilt at: 01/02/24 03:00:01: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/flake.nix b/flake.nix # index 62540ef..a45e8fe 100644 # --- a/flake.nix # +++ b/flake.nix # @@ -37,7 +37,7 @@ # ./configuration.nix # # make home-manager as a module of nixos # # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch` # - }; # + } # home-manager.nixosModules.home-manager # { # home-manager.useGlobalPkgs = true;
    Automatically committed and pushed

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
           ./configuration.nix
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-          };
+          }
           home-manager.nixosModules.home-manager
           {
               home-manager.useGlobalPkgs = true;
