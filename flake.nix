b2937dbd4cd491c6ad4447fed2eb3fbdc1cab4eb
derekagraham78 <derekagraham78@icloud.com>
2024-01-02 02:27:06 -0600

    Flake rebuilt at: 01/02/24 02:27:06: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/flake.nix b/flake.nix # index b1af364..b2a35bb 100644 # --- a/flake.nix # +++ b/flake.nix # @@ -40,7 +40,7 @@ # home-manager.useUserPackages = true; # home-manager.users.dgraham = import ./home.nix; # # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix # - } # + }; # ]; # }; # };
    Automatically committed and pushed

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
             home-manager.useUserPackages = true;
             home-manager.users.dgraham = import ./home.nix;
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-          }
+          };
         ];
       };
     };
