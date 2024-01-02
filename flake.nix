b30b1ab3ce9022b4887c98aedb4afb39211348b9
derekagraham78 <derekagraham78@icloud.com>
2024-01-02 03:03:19 -0600

    Flake rebuilt at: 01/02/24 03:03:19: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/flake.nix b/flake.nix # index d58a129..1ef1e71 100644 # --- a/flake.nix # +++ b/flake.nix # @@ -36,7 +36,8 @@ # "nixos-mulder" = nixpkgs.lib.nixosSystem { # system = "x86_64-linux"; # specialArgs = inputs; # - ./configuration.nix # + modules = [ # + ./configuration.nix # # make home-manager as a module of nixos # # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch` # home-manager.nixosModules.home-manager # @@ -46,6 +47,7 @@ # home-manager.users.dgraham = import ./home.nix; # # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix # }; # + ]; # }; # }; # }
    Automatically committed and pushed

--- flake.nix
+++ flake.nix
@@ -36,7 +36,8 @@
       "nixos-mulder" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = inputs;
-          ./configuration.nix
+         modules = [ 
+           ./configuration.nix
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
@@ -46,6 +47,7 @@
               home-manager.users.dgraham = import ./home.nix;
               # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
 	  };
+         ];
       };
     };
 }
