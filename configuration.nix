fbf345c7b7d498ae284a534e8e490dcc3ad28697
derekagraham78 <derekagraham78@icloud.com>
2024-01-02 04:07:20 -0600

    Flake rebuilt at: 01/02/24 04:07:20: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index fb83937..6536c79 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -6,7 +6,19 @@ # lib, # pkgs, # ... # -}: { # +}: # +let # + flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz"; # + # + hyprland-flake = (import flake-compat { # + src = builtins.fetchTarball "https://github.com/hyprwm/Hyprland/archive/master.tar.gz"; # + }).defaultNix; # +in { # + programs.hyprland = { # + enable = true; # + package = hyprland-flake.packages.${pkgs.system}.hyprland; # + }; # + { # imports = [ # # Include the results of the hardware scan. # ./hardware-configuration.nix
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,19 @@
   lib,
   pkgs,
   ...
-}: {
+}:
+let
+  flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
+
+  hyprland-flake = (import flake-compat {
+    src = builtins.fetchTarball "https://github.com/hyprwm/Hyprland/archive/master.tar.gz";
+  }).defaultNix;
+in {
+  programs.hyprland = {
+    enable = true;
+    package = hyprland-flake.packages.${pkgs.system}.hyprland;
+  };
+ {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
