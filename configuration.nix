f0c1a727f11241622bb08345996c36bf11169f39
derekagraham78 <derekagraham78@icloud.com>
2024-01-02 15:43:28 -0600

    Flake rebuilt at: 01/02/24 15:43:28: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index a05bb80..0d799c5 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -221,6 +221,7 @@ # # $ nix search wget # environment.systemPackages = with pkgs; [ # # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. # + killall # swaylock-effects # pavucontrol # rofi-wayland
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -221,6 +221,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    killall
     swaylock-effects
     pavucontrol
     rofi-wayland
