2185141f94bef5c9cd778c53b8a96cf368715101
Derek Graham <derekagraham78@icloud.com>
2024-01-03 00:19:50 -0600

    modified:   configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -226,6 +226,7 @@
   environment.systemPackages = with pkgs; [
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     killall
+    klibcShrunk
     swaylock-effects
     swayidle
     greetd.gtkgreet
