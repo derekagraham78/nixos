1e1ef26a9a5d34685ab8e6b49b41df93a85a01ae
Derek Graham <derekagraham78@icloud.com>
2024-01-06 04:20:00 -0600

    modified:   home.nix

--- home.nix
+++ home.nix
@@ -3,6 +3,33 @@
   pkgs,
   ...
 }: {
+  wayland.windowManager.hyprland.settings = {
+    decoration = {
+      shadow_offset = "0 5";
+      "col.shadow" = "rgba(00000099)";
+    };
+
+    "$mod" = "SUPER";
+
+    bindm = [
+      # mouse movements
+      "$mod, mouse:272, movewindow"
+      "$mod, mouse:273, resizewindow"
+      "$mod ALT, mouse:272, resizewindow"
+    ];
+  };
+  home.file."~/.config/hypr/hyprland.conf".text = ''
+    decoration {
+      shadow_offset = 0 5
+      col.shadow = rgba(00000099)
+    }
+
+    $mod = SUPER
+
+    bindm = $mod, mouse:272, movewindow
+    bindm = $mod, mouse:273, resizewindow
+    bindm = $mod ALT, mouse:272, resizewindow
+  '';
   # Optional
   # Whether to enable hyprland-session.target on hyprland startup
   systemd.enable = true;
