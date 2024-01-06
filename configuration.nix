8d722e9487832c056ce4a7fe49ec35713e8759b0
derekagraham78 <derekagraham78@icloud.com>
2024-01-05 23:02:55 -0600

    Flake rebuilt at: 01/05/24 23:02:55: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index 46d14f0..082107c 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -154,8 +154,8 @@ in { # # Enable the X11 windowing system. # services.flatpak.enable = true; # xdg.portal.enable = true; # - xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-hyprland]; # - xdg.portal.config.common.default = ["hyprland" "kde" "gtk"]; # + # xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-hyprland]; # + xdg.portal.config.common.default = ["kde" "gtk"]; # services.xserver.xkb.model = "Logitech K270"; # # # Configure keymap in X11
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -154,8 +154,8 @@ in {
   # Enable the X11 windowing system.
   services.flatpak.enable = true;
   xdg.portal.enable = true;
-  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-hyprland];
-  xdg.portal.config.common.default = ["hyprland" "kde" "gtk"];
+  #  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-hyprland];
+  xdg.portal.config.common.default = ["kde" "gtk"];
   services.xserver.xkb.model = "Logitech K270";
 
   # Configure keymap in X11
