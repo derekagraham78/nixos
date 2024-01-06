6a8df5db307f434039f73f552b18334524127725
Derek Graham <derekagraham78@icloud.com>
2024-01-06 02:45:16 -0600

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     hyprland.url = "github:hyprwm/Hyprland";
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
+    # hyprland-plugins = {
+    #   url = "github:hyprwm/hyprland-plugins";
+    #   inputs.hyprland.follows = "hyprland";
+    # };
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     # The nixpkgs entry in the flake registry.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
