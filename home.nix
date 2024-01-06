728b883d49c7d3f1efc3cbfd12087d84991b160f
Derek Graham <derekagraham78@icloud.com>
2024-01-06 02:43:01 -0600

    modified:   home.nix

--- home.nix
+++ home.nix
@@ -32,16 +32,15 @@ in
       enable = true;
       plugins = [
         inputs.hyprland-plugins.packages.${pkgs.system}.hyprload
-        inputs.hyprland-plugins.packages.${pkgs.system}.split-monitor-workspaces
-        inputs.hyprland-plugins.packages.${pkgs.system}.hyprNStack
-        inputs.hyprland-plugins.packages.${pkgs.system}.hyprRiver
-        inputs.hyprland-plugins.packages.${pkgs.system}.hyprfocus
-        inputs.hyprland-plugins.packages.${pkgs.system}.hyprland-dwindle-autogroup
-        inputs.hyprland-plugins.packages.${pkgs.system}.hyprland-virtual-desktops
-        inputs.hyprland-plugins.packages.${pkgs.system}.Hypr-DarkWindow
-        inputs.hyprland-plugins.packages.${pkgs.system}.hyprtrails
-
-        inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
+        #     inputs.hyprland-plugins.packages.${pkgs.system}.split-monitor-workspaces
+        #     inputs.hyprland-plugins.packages.${pkgs.system}.hyprNStack
+        #     inputs.hyprland-plugins.packages.${pkgs.system}.hyprRiver
+        #     inputs.hyprland-plugins.packages.${pkgs.system}.hyprfocus
+        #     inputs.hyprland-plugins.packages.${pkgs.system}.hyprland-dwindle-autogroup
+        #     inputs.hyprland-plugins.packages.${pkgs.system}.hyprland-virtual-desktops
+        #     inputs.hyprland-plugins.packages.${pkgs.system}.Hypr-DarkWindow
+        #     inputs.hyprland-plugins.packages.${pkgs.system}.hyprtrails
+        #      inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
         # ...
       ];
       settings = {
