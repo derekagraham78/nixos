6f82bd7e052e4366d45afd0e9f111f3b451649fb
derekagraham78 <derekagraham78@icloud.com>
2024-01-06 17:58:08 -0600

    Flake rebuilt at: 01/06/24 17:58:08: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index 6227014..29cd660 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -66,7 +66,10 @@ # # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain"; # virtualisation.docker.enable = true; # # Enable networking # - # + services.plex = { # + enable = true; # + openFirewall = true; # + }; # networking.networkmanager.enable = true; # networking. enableIPv6 = false; # # Set your time zone.
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -66,7 +66,10 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   virtualisation.docker.enable = true;
   # Enable networking
-
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
   networking.networkmanager.enable = true;
   networking. enableIPv6 = false;
   # Set your time zone.
