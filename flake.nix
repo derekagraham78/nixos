2318b2e8007710d3353775bd2ef90e60fb13a7b8
Derek Graham <derekagraham78@icloud.com>
2024-01-06 02:51:58 -0600

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -30,7 +30,8 @@
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
-            home-manager.users.dgraham = {pkgs, ...}: import ./home.nix;
+            home-manager.users.dgraham = import ./home.nix;
+            #{pkgs, ...}:
             home-manager.useUserPackages = true;
             #home-manager.extraSpecialArgs = {inherit inputs;};
           }
