9e8d492b6145f76e55d1d66b9f0dd6bc24d87236
Derek Graham <derekagraham78@icloud.com>
2024-01-06 03:47:47 -0600

    modified:   home.nix

--- home.nix
+++ home.nix
@@ -19,7 +19,7 @@
   };
   wayland.windowManager.hyprland = {
     plugins = [
-      inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
+      inputs.hyprland-plugins.packages.${pkgs.system}.hyprtrails
     ];
   };
 
