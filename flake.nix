ae22b8953e5a05504ec81409557ce23d7b19658c
derekagraham78 <derekagraham78@icloud.com>
2024-01-05 23:03:53 -0600

    Flake rebuilt at: 01/05/24 23:03:53: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/flake.nix b/flake.nix # index 217a1e6..56b89fc 100644 # --- a/flake.nix # +++ b/flake.nix # @@ -15,13 +15,13 @@ # home-manager = { # url = "github:nix-community/home-manager"; # inputs.nixpkgs.follows = "nixpkgs"; # - # wayland.windowManager.hyprland = { # - # enable = true; # - # ... # - # plugins = [ # - # inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars # - # ]; # - # }; # + wayland.windowManager.hyprland = { # + enable = true; # + # ... # + plugins = [ # + inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars # + ]; # + }; # }; # }; # outputs = {
    Automatically committed and pushed

--- flake.nix
+++ flake.nix
@@ -15,13 +15,13 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-      #      wayland.windowManager.hyprland = {
-      #        enable = true;
-      # ...
-      #        plugins = [
-      #          inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
-      #        ];
-      #      };
+      wayland.windowManager.hyprland = {
+        enable = true;
+        # ...
+        plugins = [
+          inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
+        ];
+      };
     };
   };
   outputs = {
