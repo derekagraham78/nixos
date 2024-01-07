4bac9a7deac18295aa02aeeded2d00f12c9453fe
derekagraham78 <derekagraham78@icloud.com>
2024-01-06 18:11:39 -0600

    Flake rebuilt at: 01/06/24 18:11:39: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/home.nix b/home.nix # index 316d4f2..ae0d8e0 100644 # --- a/home.nix # +++ b/home.nix # @@ -3,21 +3,21 @@ # pkgs, # ... # }: { # -# windowManager.hyprland.settings = { # -# decoration = { # -# shadow_offset = "0 5"; # -# "col.shadow" = "rgba(00000099)"; # -# }; # + # windowManager.hyprland.settings = { # + # decoration = { # + # shadow_offset = "0 5"; # + # "col.shadow" = "rgba(00000099)"; # + # }; # # - "$mod" = "SUPER"; # + "$mod" = "SUPER"; # # - bindm = [ # - # mouse movements # - "$mod, mouse:272, movewindow" # - "$mod, mouse:273, resizewindow" # - "$mod ALT, mouse:272, resizewindow" # - ]; # - }; # + bindm = [ # + # mouse movements # + "$mod, mouse:272, movewindow" # + "$mod, mouse:273, resizewindow" # + "$mod ALT, mouse:272, resizewindow" # + ]; # + # }; # home.file."~/.config/hypr/hyprland.conf".text = '' # decoration { # shadow_offset = 0 5
    Automatically committed and pushed

--- home.nix
+++ home.nix
@@ -3,21 +3,21 @@
   pkgs,
   ...
 }: {
-#  windowManager.hyprland.settings = {
-#    decoration = {
-#      shadow_offset = "0 5";
-#      "col.shadow" = "rgba(00000099)";
-#    };
+  #  windowManager.hyprland.settings = {
+  #    decoration = {
+  #      shadow_offset = "0 5";
+  #      "col.shadow" = "rgba(00000099)";
+  #    };
 
-    "$mod" = "SUPER";
+  "$mod" = "SUPER";
 
-    bindm = [
-      # mouse movements
-      "$mod, mouse:272, movewindow"
-      "$mod, mouse:273, resizewindow"
-      "$mod ALT, mouse:272, resizewindow"
-    ];
-  };
+  bindm = [
+    # mouse movements
+    "$mod, mouse:272, movewindow"
+    "$mod, mouse:273, resizewindow"
+    "$mod ALT, mouse:272, resizewindow"
+  ];
+  #  };
   home.file."~/.config/hypr/hyprland.conf".text = ''
     decoration {
       shadow_offset = 0 5
