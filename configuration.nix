34a9bc1f2d2120917410c898586ffbc8180f10b0
derekagraham78 <derekagraham78@icloud.com>
2024-01-06 22:21:19 -0600

    Flake rebuilt at: 01/06/24 22:21:20: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index 29cd660..a2dea58 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -231,6 +231,7 @@ # environment.systemPackages = with pkgs; [ # # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. # slack # + mullvad-vpn # gwenview # dmenu-rs # clipmenu
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -231,6 +231,7 @@
   environment.systemPackages = with pkgs; [
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     slack
+    mullvad-vpn
     gwenview
     dmenu-rs
     clipmenu
