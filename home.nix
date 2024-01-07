5f6d8d7886fbfc86789604d5d3183c7e2b712176
derekagraham78 <derekagraham78@icloud.com>
2024-01-06 18:10:08 -0600

    Flake rebuilt at: 01/06/24 18:10:08: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/home.nix b/home.nix # index db10577..316d4f2 100644 # --- a/home.nix # +++ b/home.nix # @@ -3,11 +3,11 @@ # pkgs, # ... # }: { # - windowManager.hyprland.settings = { # - decoration = { # - shadow_offset = "0 5"; # - "col.shadow" = "rgba(00000099)"; # - }; # +# windowManager.hyprland.settings = { # +# decoration = { # +# shadow_offset = "0 5"; # +# "col.shadow" = "rgba(00000099)"; # +# }; # # "$mod" = "SUPER"; #
    Automatically committed and pushed

--- home.nix
+++ home.nix
@@ -3,11 +3,11 @@
   pkgs,
   ...
 }: {
-  windowManager.hyprland.settings = {
-    decoration = {
-      shadow_offset = "0 5";
-      "col.shadow" = "rgba(00000099)";
-    };
+#  windowManager.hyprland.settings = {
+#    decoration = {
+#      shadow_offset = "0 5";
+#      "col.shadow" = "rgba(00000099)";
+#    };
 
     "$mod" = "SUPER";
 
