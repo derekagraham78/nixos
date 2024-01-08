3166c2051a6dffa1973ec3da1f8ac9597991a5e7
derekagraham78 <derekagraham78@icloud.com>
2024-01-08 00:13:34 -0600

    Flake rebuilt at: 01/08/24 00:13:34: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index d59bf61..4c01c80 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -18,7 +18,7 @@ # ]; # programs.hyprland = { # enable = true; # - package = inputs.hyprland.packages.${pkgs.system}.hyprland; # + package = inputs.hyprland.packages.${pkgs.system}.Hyprland; # }; # vscode.user = "dgraham"; # vscode.homeDir = "/home/dgraham";
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -18,7 +18,7 @@
   ];
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    package = inputs.hyprland.packages.${pkgs.system}.Hyprland;
   };
   vscode.user = "dgraham";
   vscode.homeDir = "/home/dgraham";
