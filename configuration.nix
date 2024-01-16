d313612fed6d5fae592f9cb985b42632641f5cb4
derekagraham78 <derekagraham78@icloud.com>
2024-01-16 00:37:43 -0600

    Flake rebuilt at: 01/16/24 00:37:43: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index e6c0810..fb03d0a 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -53,7 +53,6 @@ # #networking.wireless.enable = true; # Enables wireless support via wpa_supplicant. # # Printer # services.xrdp.enable = true; # - services.xrdp.port = "5901"; # services.printing.drivers = [pkgs.brlaser]; # services.dbus.packages = with pkgs; [ # xfce.xfconf
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -53,7 +53,6 @@
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Printer
   services.xrdp.enable = true;
-  services.xrdp.port = "5901";
   services.printing.drivers = [pkgs.brlaser];
   services.dbus.packages = with pkgs; [
     xfce.xfconf
