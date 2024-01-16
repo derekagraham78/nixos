014b74ad2f1c2b167cb111e1a551a4998011314d
derekagraham78 <derekagraham78@icloud.com>
2024-01-16 00:35:50 -0600

    Flake rebuilt at: 01/16/24 00:35:50: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index b316f9a..e6c0810 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -73,23 +73,6 @@ # # Set your time zone. # time.timeZone = "America/Chicago"; # programs.sway.enable = true; # - secruity.polkit.enable = true; # - security.polkit.extraConfig = '' # - polkit.addRule(function(action, subject) { # - if ( # - subject.isInGroup("users") # - && ( # - action.id == "org.freedesktop.login1.reboot" || # - action.id == "org.freedesktop.login1.reboot-multiple-sessions" || # - action.id == "org.freedesktop.login1.power-off" || # - action.id == "org.freedesktop.login1.power-off-multiple-sessions" # - ) # - ) # - { # - return polkit.Result.YES; # - } # - }) # - ''; # security.pam.services.swaylock.fprintAuth = false; # systemd.services.backupmyconfs = { # path = [pkgs.zsh];
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -73,23 +73,6 @@
   # Set your time zone.
   time.timeZone = "America/Chicago";
   programs.sway.enable = true;
-  secruity.polkit.enable = true;
-  security.polkit.extraConfig = ''
-    polkit.addRule(function(action, subject) {
-      if (
-        subject.isInGroup("users")
-          && (
-            action.id == "org.freedesktop.login1.reboot" ||
-            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-            action.id == "org.freedesktop.login1.power-off" ||
-            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
-          )
-        )
-      {
-        return polkit.Result.YES;
-      }
-    })
-  '';
   security.pam.services.swaylock.fprintAuth = false;
   systemd.services.backupmyconfs = {
     path = [pkgs.zsh];
