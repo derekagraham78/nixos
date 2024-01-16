f32925ff4d6a5490e66d5292671d537f99f16ceb
derekagraham78 <derekagraham78@icloud.com>
2024-01-15 22:40:43 -0600

    Flake rebuilt at: 01/15/24 22:40:43: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index a73046b..42eb332 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -223,8 +223,6 @@ # # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. # slack # wev # - nmap # - netbox # ox # floorp # deluge-gtk
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -223,8 +223,6 @@
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     slack
     wev
-    nmap
-    netbox
     ox
     floorp
     deluge-gtk
