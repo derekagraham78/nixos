c56329cbf6d7f99c77eebb01cb8d6e022895e615
derekagraham78 <derekagraham78@icloud.com>
2024-01-23 15:53:49 -0600

    Flake rebuilt at: 01/23/24 15:53:49: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/flake.lock b/flake.lock # index b8da090..7530a21 100644 # --- a/flake.lock # +++ b/flake.lock # @@ -200,16 +200,16 @@ # }, # "nixpkgs_3": { # "locked": { # - "lastModified": 1705856552, # - "narHash": "sha256-JXfnuEf5Yd6bhMs/uvM67/joxYKoysyE3M2k6T3eWbg=", # + "lastModified": 1705978183, # + "narHash": "sha256-p8M+ifmGRApkMG5M3t2OFJF38+FJq8VElZtEQm1utP4=", # "owner": "NixOS", # "repo": "nixpkgs", # - "rev": "612f97239e2cc474c13c9dafa0df378058c5ad8d", # + "rev": "0e0b16295676114d2994db7f620b52d7958dc33c", # "type": "github" # }, # "original": { # "owner": "NixOS", # - "ref": "nixos-unstable", # + "ref": "nixos-unstable-small", # "repo": "nixpkgs", # "type": "github" # } # diff --git a/flake.nix b/flake.nix # index da3a84e..1138b01 100644 # --- a/flake.nix # +++ b/flake.nix # @@ -6,7 +6,7 @@ # # nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions"; # # The nixpkgs entry in the flake registry. # - nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # + nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small"; # home-manager = { # url = "github:nix-community/home-manager"; # inputs.nixpkgs.follows = "nixpkgs";
    Automatically committed and pushed

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     # The nixpkgs entry in the flake registry.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
