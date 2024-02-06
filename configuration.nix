702d3a97f46328fb577444947ef065b1180ca4ef
derekagraham78 <derekagraham78@icloud.com>
2024-02-06 00:15:00 -0600

    Flake rebuilt at: 02/06/24 00:15:00: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index 14992e6..c2c9ef3 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -62,6 +62,15 @@ # #networking.wireless.enable = true; # Enables wireless support via wpa_supplicant. # # Printer # services = { # + cockpit = { # + enable = true; # + port = 9090; # + settings = { # + WebService = { # + AllowUnencrypted = true; # + }; # + }; # + }; # tailscale.enable = true; # openssh = { # enable = true; # @@ -273,6 +282,7 @@ # # $ nix search wget # environment.systemPackages = with pkgs; [ # # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. # + cockpit # gnome.gnome-disk-utility # gparted # whois
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -62,6 +62,15 @@
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Printer
   services = {
+    cockpit = {
+      enable = true;
+      port = 9090;
+      settings = {
+        WebService = {
+          AllowUnencrypted = true;
+        };
+      };
+    };
     tailscale.enable = true;
     openssh = {
       enable = true;
@@ -273,6 +282,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    cockpit
     gnome.gnome-disk-utility
     gparted
     whois
