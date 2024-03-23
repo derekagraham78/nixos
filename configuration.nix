6d9fabde0b1977f7d2578e1458d7056d67c7525e
derekagraham78 <derekagraham78@icloud.com>
2024-03-23 18:43:59 -0500

    Flake rebuilt at: 03/23/24 18:43:59: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index a309040..2874de7 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -48,7 +48,7 @@ # systemd-boot.enable = true; # efi.canTouchEfiVariables = true; # }; # - kernelPackages = pkgs.linuxPackages_zen; # + kernelPackages = linuxPackages_zen; # kernelModules = ["drivetemp"]; # kernelParams = ["reboot=acpi" "coretemp"]; # };
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_zen;
+    kernelPackages = linuxPackages_zen;
     kernelModules = ["drivetemp"];
     kernelParams = ["reboot=acpi" "coretemp"];
   };
