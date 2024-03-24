4a226d3a29fa77a5c818d480b3a2fc5d499c2006
derekagraham78 <derekagraham78@icloud.com>
2024-03-23 19:00:10 -0500

    Flake rebuilt at: 03/23/24 19:00:10: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index 1b77ec2..316f1d1 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -48,7 +48,7 @@ # systemd-boot.enable = true; # efi.canTouchEfiVariables = true; # }; # - kernelPackages = pkgs.linuxPackages-rt_latest; # + kernelPackages = linuxPackages-rt_latest; # kernelModules = ["drivetemp"]; # kernelParams = ["reboot=acpi" "coretemp"]; # };
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages-rt_latest;
+    kernelPackages = linuxPackages-rt_latest;
     kernelModules = ["drivetemp"];
     kernelParams = ["reboot=acpi" "coretemp"];
   };
