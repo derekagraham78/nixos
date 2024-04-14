81a7a494fb07d77d125595a24bac5916c3f3c359
derekagraham78 <derekagraham78@icloud.com>
2024-04-14 14:03:45 -0500

    Flake rebuilt at: 04/14/24 14:03:46: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index 4e291fe..fe19eda 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -286,6 +286,7 @@ # environment.systemPackages = with pkgs; [ # # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. # microsoft-edge # + altserver-linux # element-desktop # element-desktop-wayland # element-web # diff --git a/flake.lock b/flake.lock # index f45cec2..93c14f1 100644 # --- a/flake.lock # +++ b/flake.lock # @@ -41,11 +41,11 @@ # ] # }, # "locked": { # - "lastModified": 1712759992, # - "narHash": "sha256-2APpO3ZW4idlgtlb8hB04u/rmIcKA8O7pYqxF66xbNY=", # + "lastModified": 1713077896, # + "narHash": "sha256-Noot8H0EZEAFRQWyGxh9ryvhK96xpIqKbh78X447JWs=", # "owner": "nix-community", # "repo": "home-manager", # - "rev": "31357486b0ef6f4e161e002b6893eeb4fafc3ca9", # + "rev": "630a0992b3627c64e34f179fab68e3d48c6991c0", # "type": "github" # }, # "original": { # @@ -61,11 +61,11 @@ # "nixpkgs": "nixpkgs" # }, # "locked": { # - "lastModified": 1712798884, # - "narHash": "sha256-Q1qzg/UTsCdRx4g4rlweDL2yBQFnevsXTr74ONGoGm4=", # + "lastModified": 1713059405, # + "narHash": "sha256-c26me7SL0InxZib6I/GswSSUm7wt7FKxSb3InREn78I=", # "owner": "nix-community", # "repo": "nix-vscode-extensions", # - "rev": "df5f35ed20d9f4fd64429c8e8c0bde079702f767", # + "rev": "f08de01bd9a9e68bcd64002dc6c91a149ffa06f2", # "type": "github" # }, # "original": { # @@ -92,11 +92,11 @@ # }, # "nixpkgs_2": { # "locked": { # - "lastModified": 1712608508, # - "narHash": "sha256-vMZ5603yU0wxgyQeHJryOI+O61yrX2AHwY6LOFyV1gM=", # + "lastModified": 1712791164, # + "narHash": "sha256-3sbWO1mbpWsLepZGbWaMovSO7ndZeFqDSdX0hZ9nVyw=", # "owner": "NixOS", # "repo": "nixpkgs", # - "rev": "4cba8b53da471aea2ab2b0c1f30a81e7c451f4b6", # + "rev": "1042fd8b148a9105f3c0aca3a6177fd1d9360ba5", # "type": "github" # }, # "original": {
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -286,6 +286,7 @@
   environment.systemPackages = with pkgs; [
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     microsoft-edge
+    altserver-linux
     element-desktop
     element-desktop-wayland
     element-web
