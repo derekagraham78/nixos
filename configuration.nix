ad71431a26570a0155f06d18728322ad1e63088b
derekagraham78 <derekagraham78@icloud.com>
2024-05-02 20:40:28 -0500

    Flake rebuilt at: 05/02/24 20:40:28: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index e5d3c0a..d3b4e2e 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -63,21 +63,20 @@ # #networking.wireless.enable = true; # Enables wireless support via wpa_supplicant. # # Printer # services = { # -home-assistant = { # - enable = true; # - extraComponents = [ # - # Components required to complete the onboarding # - "esphome" # - "met" # - "radio_browser" # - ]; # - config = { # - # Includes dependencies for a basic setup # - # https://www.home-assistant.io/integrations/default_config/ # - default_config = {}; # + home-assistant = { # + enable = true; # + extraComponents = [ # + # Components required to complete the onboarding # + "esphome" # + "met" # + "radio_browser" # + ]; # + config = { # + # Includes dependencies for a basic setup # + # https://www.home-assistant.io/integrations/default_config/ # + default_config = {}; # + }; # }; # - }; # -} # avahi = { # enable = true; # nssmdns4 = true; # diff --git a/docker/homebridge.yml b/docker/homebridge.yml # new file mode 100644 # index 0000000..4691e44 # --- /dev/null # +++ b/docker/homebridge.yml # @@ -0,0 +1,21 @@ # +version: '2' # +services: # + homebridge: # + image: homebridge/homebridge:latest # + restart: always # + devices: # + - /dev/video0:/dev/video0 # + network_mode: host # + volumes: # + - ./volumes/homebridge:/homebridge # + logging: # + driver: json-file # + options: # + max-size: "10mb" # + max-file: "1" # + healthcheck: # + test: curl --fail localhost:8581 || exit 1 # + interval: 60s # + retries: 5 # + start_period: 300s # + timeout: 2s
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -63,21 +63,20 @@
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Printer
   services = {
-home-assistant = {
-    enable = true;
-    extraComponents = [
-      # Components required to complete the onboarding
-      "esphome"
-      "met"
-      "radio_browser"
-    ];
-    config = {
-      # Includes dependencies for a basic setup
-      # https://www.home-assistant.io/integrations/default_config/
-      default_config = {};
+    home-assistant = {
+      enable = true;
+      extraComponents = [
+        # Components required to complete the onboarding
+        "esphome"
+        "met"
+        "radio_browser"
+      ];
+      config = {
+        # Includes dependencies for a basic setup
+        # https://www.home-assistant.io/integrations/default_config/
+        default_config = {};
+      };
     };
-  };
-}
     avahi = {
       enable = true;
       nssmdns4 = true;
