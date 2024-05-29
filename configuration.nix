0c4abe5ed561a483af5b37f27d3cf590423a820d
derekagraham78 <derekagraham78@icloud.com>
2024-05-29 18:03:40 -0500

    Flake rebuilt at: 05/29/24 18:03:40: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index 1f13ac0..0843b32 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -10,7 +10,7 @@ # imports = [ # # Include the results of the hardware scan. # ./hardware-configuration.nix # - # ./mysql.nix # + ./mysql.nix # # ./wordpress.nix # ./vscode.nix # ]; # @@ -117,33 +117,33 @@ # enable = true; # openFirewall = true; # }; # - # nginx = { # - # enable = true; # - # defaultListen = [{addr = "0.0.0.0";}]; # - # defaultSSLListenPort = 443; # - # virtualHosts."papalpenguin.com" = { # - # enableACME = true; # - # root = lib.mkDefault "/var/www/papalpenguin.com"; # - # locations."~ \\.php$".extraConfig = '' # - # fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # - # fastcgi_index index.php; # - # ''; # - # forceSSL = true; # - # serverAliases = ["www.papalpenguin.com"]; # - # }; # - # }; # - # phpfpm.pools.mypool = { # - # user = "nobody"; # - # settings = { # - # "pm" = "dynamic"; # - # "listen.owner" = config.services.nginx.user; # - # "pm.max_children" = 5; # - # "pm.start_servers" = 2; # - # "pm.min_spare_servers" = 1; # - # "pm.max_spare_servers" = 3; # - # "pm.max_requests" = 500; # - # }; # - # }; # + nginx = { # + enable = true; # + defaultListen = [{addr = "0.0.0.0";}]; # + defaultSSLListenPort = 443; # + virtualHosts."papalpenguin.com" = { # + enableACME = true; # + root = lib.mkDefault "/var/www/papalpenguin.com"; # + locations."~ \\.php$".extraConfig = '' # + fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # + fastcgi_index index.php; # + ''; # + forceSSL = true; # + serverAliases = ["www.papalpenguin.com"]; # + }; # + }; # + phpfpm.pools.mypool = { # + user = "nobody"; # + settings = { # + "pm" = "dynamic"; # + "listen.owner" = config.services.nginx.user; # + "pm.max_children" = 5; # + "pm.start_servers" = 2; # + "pm.min_spare_servers" = 1; # + "pm.max_spare_servers" = 3; # + "pm.max_requests" = 500; # + }; # + }; # libinput.enable = true; # # xserver = {
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    #    ./mysql.nix
+    ./mysql.nix
     #    ./wordpress.nix
     ./vscode.nix
   ];
@@ -117,33 +117,33 @@
       enable = true;
       openFirewall = true;
     };
-    #    nginx = {
-    #      enable = true;
-    #      defaultListen = [{addr = "0.0.0.0";}];
-    #      defaultSSLListenPort = 443;
-    #      virtualHosts."papalpenguin.com" = {
-    #        enableACME = true;
-    #        root = lib.mkDefault "/var/www/papalpenguin.com";
-    #        locations."~ \\.php$".extraConfig = ''
-    #          fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-    #          fastcgi_index index.php;
-    #        '';
-    #        forceSSL = true;
-    #        serverAliases = ["www.papalpenguin.com"];
-    #      };
-    #    };
-    #    phpfpm.pools.mypool = {
-    #      user = "nobody";
-    #      settings = {
-    #        "pm" = "dynamic";
-    #        "listen.owner" = config.services.nginx.user;
-    #        "pm.max_children" = 5;
-    #        "pm.start_servers" = 2;
-    #        "pm.min_spare_servers" = 1;
-    #        "pm.max_spare_servers" = 3;
-    #        "pm.max_requests" = 500;
-    #     };
-    #    };
+    nginx = {
+      enable = true;
+      defaultListen = [{addr = "0.0.0.0";}];
+      defaultSSLListenPort = 443;
+      virtualHosts."papalpenguin.com" = {
+        enableACME = true;
+        root = lib.mkDefault "/var/www/papalpenguin.com";
+        locations."~ \\.php$".extraConfig = ''
+          fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+          fastcgi_index index.php;
+        '';
+        forceSSL = true;
+        serverAliases = ["www.papalpenguin.com"];
+      };
+    };
+    phpfpm.pools.mypool = {
+      user = "nobody";
+      settings = {
+        "pm" = "dynamic";
+        "listen.owner" = config.services.nginx.user;
+        "pm.max_children" = 5;
+        "pm.start_servers" = 2;
+        "pm.min_spare_servers" = 1;
+        "pm.max_spare_servers" = 3;
+        "pm.max_requests" = 500;
+      };
+    };
     libinput.enable = true;
 
     xserver = {
