3fc2d42fd858d3f2f197744049072f9d124d1990
derekagraham78 <derekagraham78@icloud.com>
2024-05-29 19:32:30 -0500

    Flake rebuilt at: 05/29/24 19:32:30: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index 0f903aa..409cdcd 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -6,24 +6,24 @@ # virtualHosts."papalpenguin.com" = { # enableACME = true; # root = "/var/www/papalpenguin"; # - # locations."~ \\.php$".extraConfig = '' # - # fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # - # fastcgi_index index.php; # - # ''; # + locations."~ \\.php$".extraConfig = '' # + fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # + fastcgi_index index.php; # + ''; # forceSSL = true; # serverAliases = ["www.papalpenguin.com"]; # }; # }; # - # services.phpfpm.pools.mypool = { # - # user = "php"; # - # settings = { # - # "pm" = "dynamic"; # - # "listen.owner" = config.services.nginx.user; # - # "pm.max_children" = 5; # - # "pm.start_servers" = 2; # - # "pm.min_spare_servers" = 1; # - # "pm.max_spare_servers" = 3; # - # "pm.max_requests" = 500; # - # }; # - # }; # + services.phpfpm.pools.mypool = { # + user = "nobody"; # + settings = { # + "pm" = "dynamic"; # + "listen.owner" = config.services.nginx.user; # + "pm.max_children" = 5; # + "pm.start_servers" = 2; # + "pm.min_spare_servers" = 1; # + "pm.max_spare_servers" = 3; # + "pm.max_requests" = 500; # + }; # + }; # }
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -6,24 +6,24 @@
     virtualHosts."papalpenguin.com" = {
       enableACME = true;
       root = "/var/www/papalpenguin";
-      #      locations."~ \\.php$".extraConfig = ''
-      #       fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-      #        fastcgi_index index.php;
-      #      '';
+      locations."~ \\.php$".extraConfig = ''
+        fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+         fastcgi_index index.php;
+      '';
       forceSSL = true;
       serverAliases = ["www.papalpenguin.com"];
     };
   };
-  #  services.phpfpm.pools.mypool = {
-  #    user = "php";
-  #    settings = {
-  #      "pm" = "dynamic";
-  #      "listen.owner" = config.services.nginx.user;
-  #      "pm.max_children" = 5;
-  #      "pm.start_servers" = 2;
-  #      "pm.min_spare_servers" = 1;
-  #      "pm.max_spare_servers" = 3;
-  #      "pm.max_requests" = 500;
-  #    };
-  #  };
+  services.phpfpm.pools.mypool = {
+    user = "nobody";
+    settings = {
+      "pm" = "dynamic";
+      "listen.owner" = config.services.nginx.user;
+      "pm.max_children" = 5;
+      "pm.start_servers" = 2;
+      "pm.min_spare_servers" = 1;
+      "pm.max_spare_servers" = 3;
+      "pm.max_requests" = 500;
+    };
+  };
 }
