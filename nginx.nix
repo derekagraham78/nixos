98525beb399665519026874f20a7da92b10e6448
derekagraham78 <derekagraham78@icloud.com>
2024-05-30 05:16:52 -0500

    Flake rebuilt at: 05/30/24 05:16:52: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index d2c025b..ea9ed53 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -21,17 +21,6 @@ # virtualHosts."papalpenguin.com" = { # enableACME = true; # root = lib.mkDefault "/var/www/papalpenguin.com"; # - listen = [ # - { # - addr = "208.59.78.209"; # - port = 443; # - ssl = true; # - } # - { # - addr = "208.59.78.209"; # - port = 80; # - } # - ]; # locations."~ \\.php$".extraConfig = '' # fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # fastcgi_index index.php;
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -21,17 +21,6 @@
     virtualHosts."papalpenguin.com" = {
       enableACME = true;
       root = lib.mkDefault "/var/www/papalpenguin.com";
-      listen = [
-        {
-          addr = "208.59.78.209";
-          port = 443;
-          ssl = true;
-        }
-        {
-          addr = "208.59.78.209";
-          port = 80;
-        }
-      ];
       locations."~ \\.php$".extraConfig = ''
         fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
          fastcgi_index index.php;
