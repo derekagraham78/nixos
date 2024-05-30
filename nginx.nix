ec9b8261c15e18f3d69c389af050eaf5ed7b2031
derekagraham78 <derekagraham78@icloud.com>
2024-05-30 01:01:34 -0500

    Flake rebuilt at: 05/30/24 01:01:34: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index e582c14..a6bacd9 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -1,24 +1,24 @@ # {config, ...}: { # - services.nginx = { # - enable = true; # - defaultListen = [{addr = "0.0.0.0";}]; # - defaultSSLListenPort = 443; # - virtualHosts."papalpenguin.com" = { # - enableACME = true; # - root = "/var/www/papalpenguin.com"; # - locations."~ \\.php$".extraConfig = '' # - fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # - fastcgi_index index.php; # - ''; # - forceSSL = false; # - serverAliases = ["www.papalpenguin.com"]; # - }; # + services.nginx = { # + enable = true; # + defaultListen = [{addr = "0.0.0.0";}]; # + defaultSSLListenPort = 443; # + virtualHosts."papalpenguin.com" = { # + enableACME = true; # + root = "/var/www/papalpenguin.com"; # + locations."~ \\.php$".extraConfig = '' # + fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # + fastcgi_index index.php; # + ''; # + forceSSL = false; # + serverAliases = ["www.papalpenguin.com"]; # }; # + }; # services.phpfpm.pools.mypool = { # user = "nobody"; # settings = { # "pm" = "dynamic"; # - "listen.owner" = config.services.nginx.user;; # + "listen.owner" = config.services.nginx.user; # "pm.max_children" = 5; # "pm.start_servers" = 2; # "pm.min_spare_servers" = 1;
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -1,24 +1,24 @@
 {config, ...}: {
-    services.nginx = {
-      enable = true;
-      defaultListen = [{addr = "0.0.0.0";}];
-      defaultSSLListenPort = 443;
-      virtualHosts."papalpenguin.com" = {
-        enableACME = true;
-        root = "/var/www/papalpenguin.com";
-        locations."~ \\.php$".extraConfig = ''
-          fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-           fastcgi_index index.php;
-        '';
-        forceSSL = false;
-        serverAliases = ["www.papalpenguin.com"];
-      };
+  services.nginx = {
+    enable = true;
+    defaultListen = [{addr = "0.0.0.0";}];
+    defaultSSLListenPort = 443;
+    virtualHosts."papalpenguin.com" = {
+      enableACME = true;
+      root = "/var/www/papalpenguin.com";
+      locations."~ \\.php$".extraConfig = ''
+        fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+         fastcgi_index index.php;
+      '';
+      forceSSL = false;
+      serverAliases = ["www.papalpenguin.com"];
     };
+  };
   services.phpfpm.pools.mypool = {
     user = "nobody";
     settings = {
       "pm" = "dynamic";
-      "listen.owner" = config.services.nginx.user;;
+      "listen.owner" = config.services.nginx.user;
       "pm.max_children" = 5;
       "pm.start_servers" = 2;
       "pm.min_spare_servers" = 1;
