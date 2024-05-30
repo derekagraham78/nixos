3997357c7da2153c355ab196ee74cd18184d84ec
derekagraham78 <derekagraham78@icloud.com>
2024-05-30 06:26:06 -0500

    Flake rebuilt at: 05/30/24 06:26:06: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index cdcb1a9..ec1b9a7 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -269,6 +269,7 @@ # # $ nix search wget # environment.systemPackages = with pkgs; [ # # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. # + filezilla # idevicerestore # microsoft-edge # altserver-linux
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -269,6 +269,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    filezilla
     idevicerestore
     microsoft-edge
     altserver-linux
