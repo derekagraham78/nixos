af3ac50ab05360cacca0e9839da2d65f098cafe5
derekagraham78 <derekagraham78@icloud.com>
2024-05-30 05:57:59 -0500

    Flake rebuilt at: 05/30/24 05:57:59: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index e3d7b6d..3212727 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -19,11 +19,9 @@ # }; # "~ \.php$" = { # extraConfig = '' # - try_files $uri =404; # - fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # - fastcgi_index index.php; # - # include ${pkgs.nginx}/conf/fastcgi_params; # - # include ${pkgs.nginx}/conf/fastcgi.conf; # + try_files $uri =404; # + fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # + fastcgi_index index.php; # ''; # }; # };
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -19,11 +19,9 @@
         };
         "~ \.php$" = {
           extraConfig = ''
-                        try_files $uri =404;
-                        fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-                        fastcgi_index index.php;
-            #            include ${pkgs.nginx}/conf/fastcgi_params;
-            #            include ${pkgs.nginx}/conf/fastcgi.conf;
+            try_files $uri =404;
+            fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+            fastcgi_index index.php;
           '';
         };
       };
