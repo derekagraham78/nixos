e3dc03bcd04a2e776dc4e0f8cdb23ae64363aabc
derekagraham78 <derekagraham78@icloud.com>
2024-05-29 19:25:50 -0500

    Flake rebuilt at: 05/29/24 19:25:50: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index e3991b1..5fc3243 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -14,16 +14,16 @@ # serverAliases = ["www.papalpenguin.com"]; # }; # }; # - services.phpfpm.pools.mypool = { # - user = "php"; # - settings = { # - "pm" = "dynamic"; # - "listen.owner" = config.services.nginx.user; # - "pm.max_children" = 5; # - "pm.start_servers" = 2; # - "pm.min_spare_servers" = 1; # - "pm.max_spare_servers" = 3; # - "pm.max_requests" = 500; # - }; # +# services.phpfpm.pools.mypool = { # +# user = "php"; # +# settings = { # +# "pm" = "dynamic"; # +# "listen.owner" = config.services.nginx.user; # +# "pm.max_children" = 5; # +# "pm.start_servers" = 2; # +# "pm.min_spare_servers" = 1; # +# "pm.max_spare_servers" = 3; # +# "pm.max_requests" = 500; # +# }; # }; # }
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -14,16 +14,16 @@
       serverAliases = ["www.papalpenguin.com"];
     };
   };
-  services.phpfpm.pools.mypool = {
-    user = "php";
-    settings = {
-      "pm" = "dynamic";
-      "listen.owner" = config.services.nginx.user;
-      "pm.max_children" = 5;
-      "pm.start_servers" = 2;
-      "pm.min_spare_servers" = 1;
-      "pm.max_spare_servers" = 3;
-      "pm.max_requests" = 500;
-    };
+#  services.phpfpm.pools.mypool = {
+#    user = "php";
+#    settings = {
+#      "pm" = "dynamic";
+#      "listen.owner" = config.services.nginx.user;
+#      "pm.max_children" = 5;
+#      "pm.start_servers" = 2;
+#      "pm.min_spare_servers" = 1;
+#      "pm.max_spare_servers" = 3;
+#      "pm.max_requests" = 500;
+#    };
   };
 }
