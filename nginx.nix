d55214b1e95a22e0acc8abc70ed6cd7257c84e35
derekagraham78 <derekagraham78@icloud.com>
2024-05-30 01:28:43 -0500

    Flake rebuilt at: 05/30/24 01:28:43: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index a6bacd9..4b42923 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -1,11 +1,15 @@ # -{config, ...}: { # +{ # + config, # + lib, # + ... # +}: { # services.nginx = { # enable = true; # defaultListen = [{addr = "0.0.0.0";}]; # defaultSSLListenPort = 443; # virtualHosts."papalpenguin.com" = { # enableACME = true; # - root = "/var/www/papalpenguin.com"; # + root = lib.mkDefault "/var/www/papalpenguin.com"; # locations."~ \\.php$".extraConfig = '' # fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # fastcgi_index index.php;
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -1,11 +1,15 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   services.nginx = {
     enable = true;
     defaultListen = [{addr = "0.0.0.0";}];
     defaultSSLListenPort = 443;
     virtualHosts."papalpenguin.com" = {
       enableACME = true;
-      root = "/var/www/papalpenguin.com";
+      root = lib.mkDefault "/var/www/papalpenguin.com";
       locations."~ \\.php$".extraConfig = ''
         fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
          fastcgi_index index.php;
