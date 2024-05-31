74e76a61b5dacf963e28991808e8a7ed7a6c5d80
derekagraham78 <derekagraham78@icloud.com>
2024-05-30 20:46:26 -0500

    Flake rebuilt at: 05/30/24 20:46:26: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/flake.lock b/flake.lock # index abe46bb..877d709 100644 # --- a/flake.lock # +++ b/flake.lock # @@ -41,11 +41,11 @@ # ] # }, # "locked": { # - "lastModified": 1717052710, # - "narHash": "sha256-LRhOxzXmOza5SymhOgnEzA8EAQp+94kkeUYWKKpLJ/U=", # + "lastModified": 1717097707, # + "narHash": "sha256-HC5vJ3oYsjwsCaSbkIPv80e4ebJpNvFKQTBOGlHvjLs=", # "owner": "nix-community", # "repo": "home-manager", # - "rev": "29c69d9a466e41d46fd3a7a9d0591ef9c113c2ae", # + "rev": "0eb314b4f0ba337e88123e0b1e57ef58346aafd9", # "type": "github" # }, # "original": { # @@ -61,11 +61,11 @@ # "nixpkgs": "nixpkgs" # }, # "locked": { # - "lastModified": 1717032429, # - "narHash": "sha256-1+87CE8xOUsJChiq9aNQqWPKoWMuyurW+aXrGbMWH7I=", # + "lastModified": 1717118683, # + "narHash": "sha256-l1v1gaQGY+Sn8tSyQOtwMCGJjKvHD2Fp4JpvP1heOkE=", # "owner": "nix-community", # "repo": "nix-vscode-extensions", # - "rev": "0309d806a5431a46fb7fd81e20d7133ac8b1de55", # + "rev": "2bfc4b1c0cd10407ecea2edf26e4006a934e94e8", # "type": "github" # }, # "original": { # diff --git a/nginx.nix b/nginx.nix # index c2ed92a..188cd88 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -17,16 +17,17 @@ # rewrite ^ /index.php; # ''; # }; # -locations = { "/var/www/papalpenguin.com" autoindex on; } }; # - "~ \.php$" = { # - extraConfig = '' # - try_files $uri =404; # - fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # - fastcgi_index index.php; # - ''; # - }; # - }; # - forceSSL = true; # + # +locations = { "/var/www/papalpenguin.com" autoindex on; } }; "~ \.php$" = { location ~* \.(xml|xsl)$ { add_header Cache-Control "no-cache, # + no-store, must-revalidate, max-age=0"; expires -1; } extraConfig = '' # +location /robots.txt { add_header Cache-Control "no-cache, no-store, must-revalidate, max-age=0"; expires -1; } try_files $uri =404; # + fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; fastcgi_index index.php; # +location ~* \.(css|js|pdf)$ { add_header Cache-Control "public, must-revalidate, proxy-revalidate, immutable, max-age=2592000, # +stale-while-revalidate=86400, stale-if-error=604800"; expires 30d; } ''; location ~* # +\.(jpg|jpeg|png|gif|ico|eot|swf|svg|webp|avif|ttf|otf|woff|woff2|ogg|mp4|mpeg|avi|mkv|webm|mp3)$ { add_header Cache-Control "public, # +must-revalidate, proxy-revalidate, immutable, max-age=31536000, stale-while-revalidate=86400, stale-if-error=604800"; expires 365d; } }; # +location /wp-cron.php { add_header Cache-Control "no-cache, no-store, must-revalidate, max-age=0"; expires -1; } }; # +location = /wp-content/wp-cloudflare-super-page-cache/www.papalpenguin.com/debug.log { access_log off; deny all; } forceSSL = true; # serverAliases = ["www.papalpenguin.com"]; # }; # };
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -17,16 +17,17 @@
             rewrite ^ /index.php;
           '';
         };
-locations = { "/var/www/papalpenguin.com" autoindex on; } };
-        "~ \.php$" = {
-          extraConfig = ''
-            try_files $uri =404;
-            fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-            fastcgi_index index.php;
-          '';
-        };
-      };
-      forceSSL = true;
+
+locations = { "/var/www/papalpenguin.com" autoindex on; } }; "~ \.php$" = { location ~* \.(xml|xsl)$ { add_header Cache-Control "no-cache, 
+    no-store, must-revalidate, max-age=0"; expires -1; } extraConfig = ''
+location /robots.txt { add_header Cache-Control "no-cache, no-store, must-revalidate, max-age=0"; expires -1; } try_files $uri =404; 
+            fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; fastcgi_index index.php;
+location ~* \.(css|js|pdf)$ { add_header Cache-Control "public, must-revalidate, proxy-revalidate, immutable, max-age=2592000, 
+stale-while-revalidate=86400, stale-if-error=604800"; expires 30d; } ''; location ~* 
+\.(jpg|jpeg|png|gif|ico|eot|swf|svg|webp|avif|ttf|otf|woff|woff2|ogg|mp4|mpeg|avi|mkv|webm|mp3)$ { add_header Cache-Control "public, 
+must-revalidate, proxy-revalidate, immutable, max-age=31536000, stale-while-revalidate=86400, stale-if-error=604800"; expires 365d; } }; 
+location /wp-cron.php { add_header Cache-Control "no-cache, no-store, must-revalidate, max-age=0"; expires -1; } };
+location = /wp-content/wp-cloudflare-super-page-cache/www.papalpenguin.com/debug.log { access_log off; deny all; }      forceSSL = true;
       serverAliases = ["www.papalpenguin.com"];
     };
   };
