ac3eecbd1600873abf7c6178c7e56091b651045d
derekagraham78 <derekagraham78@icloud.com>
2024-05-31 00:24:54 -0500

    Flake rebuilt at: 05/31/24 00:24:54: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index d425897..5b380d5 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -20,16 +20,15 @@ # fastcgi_index index.php; # ''; # forceSSL = true; # - locations = { # - "= /" = { # - index = "index.php"; # - extraConfig = '' # - rewrite ^ /index.php; # - ''; # - }; # - }; # +# locations = { # +# "= /" = { # +# index = "index.php"; # +# extraConfig = '' # +# rewrite ^ /index.php; # +# ''; # +# }; # +# }; # serverAliases = ["www.papalpenguin.com"]; # - }; # }; # services.phpfpm.pools.mypool = { # user = "nginx";
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -20,16 +20,15 @@
           fastcgi_index index.php;
       '';
       forceSSL = true;
-      locations = {
-        "= /" = {
-          index = "index.php";
-          extraConfig = ''
-            rewrite ^ /index.php;
-          '';
-        };
-      };
+#      locations = {
+#        "= /" = {
+#          index = "index.php";
+#          extraConfig = ''
+#            rewrite ^ /index.php;
+#          '';
+#        };
+#      };
       serverAliases = ["www.papalpenguin.com"];
-    };
   };
   services.phpfpm.pools.mypool = {
     user = "nginx";
