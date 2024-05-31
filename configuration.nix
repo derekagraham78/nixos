1e80d7c39296c5d0e7b8695aa8a70e223f078fa4
derekagraham78 <derekagraham78@icloud.com>
2024-05-31 01:14:59 -0500

    Flake rebuilt at: 05/31/24 01:14:59: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index 125bca8..715eb57 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -281,6 +281,7 @@ # # $ nix search wget # environment.systemPackages = with pkgs; [ # # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. # + imagick # php82Extensions.imagick # filezilla # idevicerestore
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -281,6 +281,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    imagick
     php82Extensions.imagick
     filezilla
     idevicerestore
