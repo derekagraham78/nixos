54dd9520d603779faa6547fef8712c2af4966710
derekagraham78 <derekagraham78@icloud.com>
2024-06-01 05:13:03 -0500

    Flake rebuilt at: 06/01/24 05:13:03: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index 4de7a41..33fb289 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -17,8 +17,6 @@ # locations."~ \\.php$".extraConfig = '' # autoindex on; # try_files $uri =404; # - fastcgi_pass unix:/var/run/php5-fpm.sock; # - fastcgi_index index.php; # fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # fastcgi_split_path_info ^(.+\.php)(.*)$; # fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -17,8 +17,6 @@
       locations."~ \\.php$".extraConfig = ''
         autoindex on;
         try_files $uri =404;
-        fastcgi_pass unix:/var/run/php5-fpm.sock;
-        fastcgi_index index.php;
         fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
         fastcgi_split_path_info ^(.+\.php)(.*)$;
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
