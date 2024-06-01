e4768f9fabdaf18d50b336285822f33696f890bf
derekagraham78 <derekagraham78@icloud.com>
2024-06-01 14:08:26 -0500

    Flake rebuilt at: 06/01/24 14:08:26: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/flake.nix b/flake.nix # index 1e18063..908935f 100644 # --- a/flake.nix # +++ b/flake.nix # @@ -4,7 +4,7 @@ # inputs = { # nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions"; # # The nixpkgs entry in the flake registry. # - nixpkgs.url = "github:NixOS/nixpkgs/nixos"; # + nixpkgs.url = "github:NixOS/nixpkgs/nixos-small"; # home-manager = { # url = "github:nix-community/home-manager"; # inputs.nixpkgs.follows = "nixpkgs";
    Automatically committed and pushed

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     # The nixpkgs entry in the flake registry.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-small";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
