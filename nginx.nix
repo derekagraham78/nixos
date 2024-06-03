602aa24e89bdb198f2007d60294114042d4b3c0a
derekagraham78 <derekagraham78@icloud.com>
2024-06-03 17:46:52 -0500

    Flake rebuilt at: 06/03/24 17:46:52: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index fa088d8..26ce2eb 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -42,26 +42,18 @@ # enableACME = true; # root = "/var/www/papalpenguin.com"; # forceSSL = true; # - # locations."~ \\.php$".index = "index.php"; # - # locations."~ \\.php$".extraConfig = '' # - # autoindex on; # - # ''; # locations."~ \.user\.ini$".extraConfig = '' # deny all; # ''; # locations."~* /uploads/.*\.php$".extraConfig = '' # return 503; # ''; # - # locations."= /".extraConfig = '' # - # autoindex on; # - # rewrite ^ /index.php; # - # ''; # locations."/".index = "index.php"; # locations."/".extraConfig = '' # autoindex on; # fastcgi_pass unix:${config.services.phpfpm.pools.mypool.socket}; # fastcgi_index index.php; # - # + fastcgi_buffering on; # ''; # serverAliases = ["www.papalpenguin.com"]; # }; # @@ -95,8 +87,9 @@ # memcache # ]); # extraConfig = '' # - output_buffering = off # + output_buffering = on # memory_limit = 1G # + security.limit_extensions = .php .phtml # apc.enable_cli = 1 # opcache.memory_consumption=256 # opcache.interned_strings_buffer=64
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -42,26 +42,18 @@
       enableACME = true;
       root = "/var/www/papalpenguin.com";
       forceSSL = true;
-      #      locations."~ \\.php$".index = "index.php";
-      #      locations."~ \\.php$".extraConfig = ''
-      #        autoindex on;
-      #      '';
       locations."~ \.user\.ini$".extraConfig = ''
         deny all;
       '';
       locations."~* /uploads/.*\.php$".extraConfig = ''
         return 503;
       '';
-      #      locations."= /".extraConfig = ''
-      #        autoindex on;
-      #        rewrite ^ /index.php;
-      #      '';
       locations."/".index = "index.php";
       locations."/".extraConfig = ''
          autoindex on;
         fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
         fastcgi_index index.php;
-
+        fastcgi_buffering on;
       '';
       serverAliases = ["www.papalpenguin.com"];
     };
@@ -95,8 +87,9 @@
           memcache
         ]);
       extraConfig = ''
-        output_buffering = off
+        output_buffering = on
         memory_limit = 1G
+        security.limit_extensions = .php .phtml
         apc.enable_cli = 1
         opcache.memory_consumption=256
         opcache.interned_strings_buffer=64
