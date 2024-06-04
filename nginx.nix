716deb212225ec10f4c0c531c7ed3326305e42eb
derekagraham78 <derekagraham78@icloud.com>
2024-06-03 21:45:01 -0500

    Flake rebuilt at: 06/03/24 21:45:01: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index 9cf301b..2fe4fd4 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -8,6 +8,28 @@ # enable = true; # logError = "stderr error"; # recommendedOptimisation = true; # + appendHttpConfig = '' # + # Add HSTS header with preloading to HTTPS requests. # + # Adding this header to HTTP requests is discouraged # + map $scheme $hsts_header { # + https "max-age=31536000; includeSubdomains; preload"; # + } # + add_header Strict-Transport-Security $hsts_header; # + # + # Enable CSP for your services. # + #add_header Content-Security-Policy "script-src 'self'; object-src 'none'; base-uri 'none';" always; # + # Minimize information leaked to other domains # + # add_header 'Referrer-Policy' 'origin-when-cross-origin'; # + # + # Disable embedding as a frame # + add_header X-Frame-Options ALLOW; # + # + # Prevent injection of code in other mime types (XSS Attacks) # + # add_header X-Content-Type-Options nosniff; # + # + # This might create errors # + # proxy_cookie_path / "/; secure; HttpOnly; SameSite=strict"; # + ''; # defaultListen = [{addr = "0.0.0.0";}]; # defaultSSLListenPort = 443; # virtualHosts."papalpenguin.com" = {
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -8,6 +8,28 @@
     enable = true;
     logError = "stderr error";
     recommendedOptimisation = true;
+    appendHttpConfig = ''
+            # Add HSTS header with preloading to HTTPS requests.
+            # Adding this header to HTTP requests is discouraged
+            map $scheme $hsts_header {
+                https   "max-age=31536000; includeSubdomains; preload";
+            }
+            add_header Strict-Transport-Security $hsts_header;
+
+            # Enable CSP for your services.
+            #add_header Content-Security-Policy "script-src 'self'; object-src 'none'; base-uri 'none';" always;
+            # Minimize information leaked to other domains
+      #      add_header 'Referrer-Policy' 'origin-when-cross-origin';
+
+            # Disable embedding as a frame
+            add_header X-Frame-Options ALLOW;
+
+            # Prevent injection of code in other mime types (XSS Attacks)
+       #     add_header X-Content-Type-Options nosniff;
+
+            # This might create errors
+        #    proxy_cookie_path / "/; secure; HttpOnly; SameSite=strict";
+    '';
     defaultListen = [{addr = "0.0.0.0";}];
     defaultSSLListenPort = 443;
     virtualHosts."papalpenguin.com" = {
