fbd10ae8932bdcb9570b380238293482bf798ca5
derekagraham78 <derekagraham78@icloud.com>
2024-06-03 19:49:16 -0500

    Flake rebuilt at: 06/03/24 19:49:16: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/nginx.nix b/nginx.nix # index 37a6829..e11cee2 100644 # --- a/nginx.nix # +++ b/nginx.nix # @@ -90,7 +90,7 @@ # extraConfig = '' # output_buffering = on # memory_limit = 1G # - security.limit_extensions = # + # security.limit_extensions = # cgi.fix_pathinfo = 0 # #.php .php3 .php4 .php5 .php7 .html .htm .js .css .min.js # apc.enable_cli = 1
    Automatically committed and pushed

--- nginx.nix
+++ nginx.nix
@@ -90,7 +90,7 @@
       extraConfig = ''
                 output_buffering = on
                 memory_limit = 1G
-                security.limit_extensions =
+         #       security.limit_extensions =
                 cgi.fix_pathinfo = 0
         #.php .php3 .php4 .php5 .php7 .html .htm .js .css .min.js
                 apc.enable_cli = 1
