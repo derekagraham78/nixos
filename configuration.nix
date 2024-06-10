2c1fd3fa3c0e35ef09e722e374db73f9fd7c67a6
derekagraham78 <derekagraham78@icloud.com>
2024-06-09 20:01:19 -0500

    Flake rebuilt at: 06/09/24 20:01:20: Rebuilt .nix files on 'date +%F-%T' Repo auto committed and pushed Signed-off-by: derekagraham78 <derekagraham78@icloud.com> # Differences to be committed: # diff --git a/configuration.nix b/configuration.nix # index 6455e1f..61ac6e7 100644 # --- a/configuration.nix # +++ b/configuration.nix # @@ -82,6 +82,13 @@ # #networking.wireless.enable = true; # Enables wireless support via wpa_supplicant. # # Printer # services = { # + resolved = { # + enable = true; # + dnssec = true; # + domains = ["~."]; # + fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"]; # + dnsovertls = "true"; # + }; # tailscale = { # enable = true; # };
    Automatically committed and pushed

--- configuration.nix
+++ configuration.nix
@@ -82,6 +82,13 @@
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Printer
   services = {
+    resolved = {
+      enable = true;
+      dnssec = true;
+      domains = ["~."];
+      fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+      dnsovertls = "true";
+    };
     tailscale = {
       enable = true;
     };
